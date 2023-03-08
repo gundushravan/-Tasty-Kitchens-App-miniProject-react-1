a1ce6940a0deb3c5a7879e0fbced28275e413207
shravan <gundusravan123456@gmail.com>
2023-03-08 14:12:03 +0000

    testy_food

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Login from './components/Login'
+import './App.css'
+
+/* const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+] */
+
+const App = () => (
+  <div>
+    <Login />
+  </div>
+)
+
+export default App
